86bd897101571d0eb1220bcc31c68c42e5e2c535
AlexisLosenko <Losenko.alexis@gmail.com>
2019-04-05 14:33:43 +0200

    redimention image

--- visualisation/elements/header.php
+++ visualisation/elements/header.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">
-<img class="hackers" src="assets/img/hackers-poulette-logo.png" height="100" width="100" alt="Bjour">
+<img class="hackers" src="assets/img/hackers-poulette-logo.png" height="120" width="180" alt="Bjour">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
