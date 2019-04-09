8df849289dd9e6f03326685ae48e43fd63aa9128
AmauryD19 <amaurydotrice@gmail.com>
2019-04-09 22:08:08 +0200

    Bootstrap c'est pas la joie hein

--- visualisation/home.php
+++ visualisation/home.php
@@ -7,7 +7,7 @@
             <div class="buydownload">
                 <div class="row">
                     <div class="col-6">
-                        <h4>Raspberry buy</h4>
+                        <h4>Shop</h4>
                     </div>
                     <div class="col-6">
                         <h4>Download</h4>
@@ -16,84 +16,15 @@
                 </div>
             </section>
 
-        <!--MAIN-->
-        <section class="actu">
-            <div class="container main-content">
-                <div class="card-deck">
-                    <div class="card">
-                        <img class="card-img-top" src="./assets/img/Entrée.jpg" alt="Card image cap" id="img">
-                        <div class="card-body">
-                            <h5 class="card-title">Victoire</h5>
-                            <p class="card-text">
-                                    <h6>Cette année, les enfants des pays étrangés ont gagné la coupe du hackers poulettes, mais on ne sait pas quel pays, c'est triste.</h6>
-
-                            </p>
-
-                        </div>
-                    </div>
-                    <div class="card">
-                        <img class="card-img-top" src="./assets/img/Plat.jpg" alt="Card image cap" id="img">
-                        <div class="card-body">
-                            <h5 class="card-title">POney</h5>
-                            <p class="card-text">
-                                    <h6>Manger des cailloux, c'est bon, il parait que manger de la soupe de cailloux permet de vivre bien plus longtemps</h6>
-                            </p>
-                        </div>
-                    </div>
-                    <div class="card">
-                        <img class="card-img-top" src="./assets/img/Dessert.jpg" alt="Card image cap" id="img">
-                        <div class="card-body">
-                            <h5 class="card-title">Marcelino</h5>
-                            <p class="card-text">
-                                    <h6>Il est beau le ciel, vous ne trouvez pas?</h6>
-                                    </p>
-
-                        </div>
-                    </div>
-                </div>
-              </div>
-        </section>
-
-        <section class="section-lien">
-          <div class="container main-content">
-              <div class="card-deck">
-                  <div class="card">
-              <img class="card-img-top" src="./assets/img/Dessert.jpg" alt="Card image cap" id="img">
-              <div class="card-body">
-                  <h5 class="card-title">Marcelino</h5>
-                  <p class="card-text">
-                    <a class="btn btn-primary btn-lg" href="#" role="button">News</a>
-                  </p>
-                </div>
-              </div>
-            <div class="card">
-                <img class="card-img-top" src="./assets/img/Dessert.jpg" alt="Card image cap" id="img">
-                <div class="card-body">
-                    <h5 class="card-title">Marcelino</h5>
-                    <p class="card-text">
-                      <a class="btn btn-primary btn-lg" href="#" role="button">News</a>
-                    </p>
-                  </div>
-                </div>
-              <div class="card">
-                  <img class="card-img-top" src="./assets/img/Dessert.jpg" alt="Card image cap" id="img">
-                  <div class="card-body">
-                      <h5 class="card-title">Marcelino</h5>
-                      <p class="card-text">
-                        <a class="btn btn-primary btn-lg" href="#" role="button">News</a>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              </section>
-
+          <div class="container">
+            <img class="raspberry" src="assets/img/raspberry.svg.png" height="250" width="250" alt="logo Hackers Poulette">
+          </div>
 
               <section class="Second">
                     <div class="Formulrie">
                         <div class="row">
                             <div class="col-6">
-                                <h4>Formulaire</h4>
+                                <h4>Support</h4>
                             </div>
                             <div class="col-6">
                                 <h4>Galerie</h4>
