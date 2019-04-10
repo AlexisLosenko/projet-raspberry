6bf9e1f3fcfc8126cfaddaacc64f4b52bf839b46
AmauryD19 <amaurydotrice@gmail.com>
2019-04-10 13:48:43 +0200

    Update global.php

--- functions/global.php
+++ functions/global.php
@@ -1,6 +1,6 @@
 <?php
 $items = array('home','buy','download','education','Help me','news','project');
-$link = 'http://localhost/projet-raspberry/index.php';
+$link = 'https://hdot.biz/projet-raspberry/';
 function menu($array,$link){
     foreach($array as $val){
         echo '<a class="nav-item nav-link" href='.$link.'?page='. $val .' ">'. $val .'</a>';
